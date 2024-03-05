0777faf7ae8cc0ad87cbcb0d4b325bf06065dbbe
MollikaComputer Provider <mollikacomputer3@gmail.com>
2024-03-05 09:12:24 +0600

    footer implement

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/shared/Navbar";
 import Footer from "@/components/shared/Footer";
-
+import Container from '@mui/material/Container';
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -15,7 +15,9 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         <Navbar/>
-        {children}
+        <Container className="min-h-screen">
+          {children}
+        </Container>
         <Footer/>
         </body>
     </html>
--- src/components/shared/Footer.js
+++ src/components/shared/Footer.js
@@ -1,34 +1,78 @@
 import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 import { IconButton } from '@mui/material';
-import Stack from '@mui/material/Stack';
 
 // icons
 import FacebookIcon from '@mui/icons-material/Facebook';
+import Link from 'next/link';
+
+const navItems = [
+    {
+    route:"Home",
+    pathname:'/',
+    },
+    {
+    route:"Pages",
+    pathname:'/pages',
+    },
+    {
+    route:"Category",
+    pathname:'/category',
+    },
+    {
+    route:"News",
+    pathname:'/news',
+    },
+    {
+    route:"About",
+    pathname:'/about',
+    },
+    {
+    route:"Contact",
+    pathname:'/contact',
+    },
+];
+
 const Footer = () => {
     return (
-        <Box className="bg-black px-2 py-10">
-            <Container className='text-center'>
-                <Box sx={{
-                "& svg":{color:"white"}}}
-                >
-                <IconButton>
-                        <FacebookIcon></FacebookIcon>
-                </IconButton>
-                <IconButton>
-                        <FacebookIcon></FacebookIcon>
-                </IconButton>
-                <IconButton>
-                        <FacebookIcon></FacebookIcon>
-                </IconButton>
-                <IconButton>
-                        <FacebookIcon></FacebookIcon>
-                </IconButton>
-            </Box>
-
-            </Container>
+        <div className="w-full bg-black px-2 py-10">
+        <Container>
+            <Box>
+            <Box className=' text-center' sx={{
+            "& svg":{color:"white"}}}
+            >
+            <IconButton>
+                    <FacebookIcon></FacebookIcon>
+            </IconButton>
+            <IconButton>
+                    <FacebookIcon></FacebookIcon>
+            </IconButton>
+            <IconButton>
+                    <FacebookIcon></FacebookIcon>
+            </IconButton>
+            <IconButton>
+                    <FacebookIcon></FacebookIcon>
+            </IconButton>
         </Box>
+
+        </Box>
+        <Box className="w-full text-center">
+            {navItems.map((item) => (
+                <Link key={item} href={item.pathname} >
+                <Button className="text-white">
+                    {item.route}
+                </Button>
+                </Link>
+            ))}
+        </Box>
+        
+        </Container>
+        <Typography color="gray" variant='body2' textAlign="center" >
+           @ Newspaper design by Ranjit
+        </Typography>
+        </div>
     );
 };
 
