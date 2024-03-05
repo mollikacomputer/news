0b704356b1b49aad399d9d13912f9521def54619
MollikaComputer Provider <mollikacomputer3@gmail.com>
2024-03-05 20:32:25 +0600

    add latest news static component

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,7 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={inter.className}>12
         <Navbar/>
         <Container className="min-h-screen">
           {children}
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,19 @@
 
-
+import LatestNews from "@/components/ui/LatestNews/LatestNews";
+import {Grid} from "@mui/material";
 const HomePage = () => {
   return (
     <div>
-      <h1> Welcome to news homepage</h1>
+      <Grid container spacing={2}>
+        <Grid item xs={8}>
+          <LatestNews/>
+          <LatestNews/>
+          <LatestNews/>
+        </Grid>
+        <Grid item xs={4}>
+          <h1> Side Bar</h1>
+        </Grid>
+      </Grid>
     </div>
   );
 };
